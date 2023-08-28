be59595672946fb1d78212c4ea488ad7d9df48eb
syur997 <110324563+syur997@users.noreply.github.com>
2023-08-28 22:53:52 +0900

    [level 0] Title: 7의 개수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.py
@@ -1,8 +1,2 @@
 def solution(array):
-    x = ''
-    for i in array:
-        x += str(i)
-    for j in x:
-        if j != '7':
-            x = x.replace(j, '')
-    return len(x)
+    return str(array).count('7')
