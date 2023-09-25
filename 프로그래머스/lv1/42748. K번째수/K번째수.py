d9da706fec02c3cc1ab2feff8142866d55a0c76b
syur997 <110324563+syur997@users.noreply.github.com>
2023-09-25 23:58:01 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,5 +1,5 @@
-def solution(array, commands):
+def solution(array, commands) :
     x = []
-    for i in range(len(commands)):
+    for i in range(len(commands)) :
         x.append(sorted(array[commands[i][0]-1:commands[i][1]])[commands[i][2]-1])
     return x
