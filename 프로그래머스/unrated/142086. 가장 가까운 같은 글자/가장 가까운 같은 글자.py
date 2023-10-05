c4ad2fc0a833fd6bffaa34c02d5cd7560ce7f01b
syur997 <110324563+syur997@users.noreply.github.com>
2023-10-05 23:46:08 +0900

    Update 가장 가까운 같은 글자.py

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.py
@@ -6,7 +6,7 @@ def solution(s):
         if char in last_pos:  # 이미 같은 문자가 앞에 나왔다면
             answer.append(i - last_pos[char])  # 현재 위치와 저장된 위치의 차이를 결과에 추가
         else:  # 처음 나온 문자라면
-            answer.append(-1)  # -1을 결과에 추가
+            answer.append(-1)  # -1 결과에 추가
         last_pos[char] = i  # 현재 문자의 위치를 저장
 
     return answer
